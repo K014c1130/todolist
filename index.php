9c7d1c5cee6ba6fd67651112cd63b790384dbbbb
Wataru <k014c1136@it-neec.jp>
2016-05-30 14:14:15 +0900

    add a line

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-CTYPE html>
+<DOCTYPE html>
 <html lang="ja">
 <head>
  <meta charset="UTF-8">
@@ -7,31 +7,32 @@ CTYPE html>
 
 <body>
   <?php
+ try{
   $dsn = 'mysql:dbname=todolist;host=localhost;charset=utf8';
   $user = 'root';
   $password = 'root';
-  $bdh = new PDO($dsn, $user, $password);
+  $dbh = new PDO($dsn, $user);
   $dbh->query('SET NAMES utf8');
   $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
   if(isset($_GET['add'])) {
-    $sql = 'INSERT INTO todolist(item) VALUES(?)';
+    $sql = 'INSERT INTO list(item) VALUES(?)';
     $stmt = $dbh->prepare($sql);
     $data[] = $_GET['name'];
     $stmt-> execute($data);
 
     $dbh = null;
   } else if(isset($_GET['delete'])) {
-    $sql = 'DELETE FROM todolist WHERE id=?';
+    $sql = 'DELETE FROM list WHERE id=?';
     $stmt = $dbh->prepare($sql);
     $data[] = $_GET['name'];
-    $stmt-> execute($data);
+    $stmt->execute($data);
 
     $dbh = null;
   }
-  $sql = 'SELECT name FROM todolist WHERE id=1';
+  $sql = 'SELECT item FROM list';
   $stmt = $dbh->prepare($sql);
-  $stmt->execute();
+  $stmt-> execute();
 
   $dbh = null;
   ?>
@@ -41,16 +42,23 @@ CTYPE html>
 </form>
 
 <form method = "get" action = "project_delete">
+<?php
 while(true) {
-  $rec =$stmt->fetch(PDO::FETCH\ASSOC);
+  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
   if($rec==false){
     break;
   }
   print $rec['item'];
+  ?>
   <input type="submit" value="delete" name="$rec['id']" style="background:url('submit_delete.jpg')" />
+  <?php
   print '</br>';
 }
-
+} catch(Exception $e) {
+ print'エラー';
+ exit();
+}
+  ?>
 </body>
 
 </html>
